e7a0d0f7c0d684d86e58cf28a01ddc4a46419735
MashkaTomat <154581342+MashkaTomat@users.noreply.github.com>
2023-12-23 03:52:38 +0700

    Add files via upload

---
+++ Task1_1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task1_1._1
+/*
+Создайте коллекцию(массив) с 10 случайными числами. Выведите в консоль
+номер минимального элемента.
+*/
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+
+            Random rnd = new Random();
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rnd.Next(-15, 11);
+            }
+
+            Console.WriteLine(nums.Min());
+        }
+    }
+}
